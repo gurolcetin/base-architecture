9686f816c1248bedee4f339bb9d8b84df0c352a7
Gürol Mehmet Çetin <gurolmehmetcetin@gmail.com>
2022-02-06 21:50:47 +0300

    Swagger documentation parameters used.

--- BaseArchitecture.API/Program.cs
+++ BaseArchitecture.API/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,7 +7,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(s =>
+{
+    s.SwaggerDoc("v1", new OpenApiInfo { Title = "Base Architecture API", Version = "v1" });
+});
 
 var app = builder.Build();
 
